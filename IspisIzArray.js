7dbb3254b1871dfdeb5a4e330f0611f599bded46
Vdoa <va960@yahoo.com>
2016-03-30 21:09:51 +0200

    Zavrsetak ispisIzArry_js

--- IspisIzArray.js
+++ IspisIzArray.js
@@ -6,24 +6,38 @@ function findItems(array,item){
 	var trazeno = [];
 	for (i=0;i<item.length+1;i++){
 		for (ind1=0; ind1 < array.length;ind1++){
-			//console.log(array[3][2]);
 			for (ind2=0;ind2<array[ind1].length; ind2++){
-				//console.log(array[ind1][ind2]);
-				//console.log(array[ind1].length);
-				
-				if (array[ind1][ind2]=item[i]){
-					trazeno.push(array[ind1][ind2]);
-					console.log(array[ind1][ind2]);
-					//return trazeno;
-					//console.log(trazeno);
+				if (array[ind1][ind2].length==3){
+					for (ind3=0; ind3<array[ind1][ind2].length; ind3++){
+						if (array[ind1][ind2][ind3]==item[i]){
+							trazeno.push(array[ind1][ind2][ind3]);
+						}
+					}
+	
 				}
+				else {
+					if (array[ind1][ind2]==item[i]){
+							trazeno.push(array[ind1][ind2]);
+					}
+				}
+					
 		   
 			}
+			
 		}	
 	}
+	
 	return trazeno;
 }
 var poziv = findItems(myArray,items);
+function printArray(array){
+	
+	for(var i = 0; i < array.length; i++){
+		console.log(array[i] );
+	}
+	
+}
+printArray(poziv);
 //console.log(poziv);
 
 
